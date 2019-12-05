68d4ee99a2f9818eeb04881b01fc061afab08218
SB <skazka064@mail.ru>
2019-12-05 23:11:01 +0400

    2

--- libs/db.php
+++ libs/db.php
@@ -4,7 +4,5 @@ $dbname = 'parser'; // название базы
 $user = "root"; // логин пользователя
 $pass = ''; // пароль
 $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
-$sth = $db->prepare("SELECT * FROM links ");
-$sth->execute();
-$l = $sth->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
